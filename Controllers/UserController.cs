262affe337bbd6809e096ddbd9f28f1b48456611
Agboji Simon <agbojisimon107@gmail.com>
2025-12-19 22:16:20 +0100

    Validate Inputs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using user.DTOs.User;
 using user.Interface;
 using user.Mappers;
@@ -22,6 +23,9 @@ namespace user.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var user = await _userRepo.GetAllAsync();
 
             var userDto = user.Select(u => u.ToUserDto());
@@ -29,9 +33,12 @@ namespace user.Controllers
             return Ok(userDto);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var user = await _userRepo.GetByIdAsync(id);
 
             if (user == null)
@@ -45,6 +52,9 @@ namespace user.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateUserDto userDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var userModel = userDto.ToUserFromCreateDto();
 
             await _userRepo.CreateAsync(userModel);
@@ -53,9 +63,12 @@ namespace user.Controllers
         }
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateUserDto userDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var user = await _userRepo.UpdateAsync(id, userDto);
             if (user == null)
             {
@@ -67,9 +80,12 @@ namespace user.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var userModel = await _userRepo.DeleteAsync(id);
             if (userModel == null)
             {
--- DTOs/User/UpdateUserDto.cs
+++ DTOs/User/UpdateUserDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace user.DTOs.User
 {
     public class UpdateUserDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Firstname must not be less than 3 characters")]
+        [MaxLength(50, ErrorMessage = "Firstname must not be greater than 50 characters")]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [MinLength(3, ErrorMessage = "Lastname must not be less than 3 characters")]
+        [MaxLength(50, ErrorMessage = "Lastname must not be greater than 50 characters")]
         public string LastName { get; set; } = string.Empty;
         public string Role { get; set; } = "Member";
     }
